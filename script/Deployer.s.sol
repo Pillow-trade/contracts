14bb88c9ecfadf8578850497714ec4b11e4960ef
chixx <lucottejeremie@gmail.com>
2025-07-05 15:26:22 +0200

    update deployer with updateted pillow params

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -1,12 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Pillow} from "../src/Pillow.sol";
+
+// base 1inch router: 
 
 contract DeployerScript is Script {
-    function setUp() public {}
+  Pillow public pillow;
 
-    function run() public {
-        vm.broadcast();
-    }
+  function setUp() public {}
+
+  function run() public {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+
+    pillow = new Pillow(
+      "Pillow",
+      "PILLOW",
+      0x0000000000000000000000000000000000000000,
+      0x694AA1769357215DE4FAC081bf1f309aDC325306,
+      0x18e5F5C1ff5e905b32CE860576031AE90E1d1336
+    );
+
+    vm.stopBroadcast();
+  }
 }
