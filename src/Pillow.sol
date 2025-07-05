6ec427097e1b39ef8a0791f323eb5af4d5fefb77
chixx <lucottejeremie@gmail.com>
2025-07-05 12:55:13 +0200

    update pillow with EulerSwap periphery

--- src/Pillow.sol
+++ src/Pillow.sol
@@ -1,25 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {AggregatorV3Interface} from "chainlink-evm/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {IEulerSwapPeriphery} from "./interfaces/IEulerSwapPeriphery.sol";
 
-contract Pillow is ERC4626 {
-    AggregatorV3Interface public immutable priceFeed;
+contract Pillow is ERC4626, Ownable(msg.sender) {
+  AggregatorV3Interface public immutable priceFeed;
+  IEulerSwapPeriphery public eulerPeriphery;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        address _asset,
-        address _priceFeed
-    ) ERC4626(IERC20(_asset)) ERC20(_name, _symbol) {
-        priceFeed = AggregatorV3Interface(_priceFeed);
-    }
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    address _asset,
+    address _priceFeed,
+    address _eulerPeriphery
+  ) ERC4626(IERC20(_asset)) ERC20(_name, _symbol) {
+    priceFeed = AggregatorV3Interface(_priceFeed);
+    eulerPeriphery = IEulerSwapPeriphery(_eulerPeriphery);
+  }
 
-    // https://docs.chain.link/data-feeds/using-data-feeds
-    function getChainlinkDataFeedLatestAnswer() public view returns (int256) {
+  function setEulerPeriphery(address _eulerPeriphery) external onlyOwner {
+    eulerPeriphery = IEulerSwapPeriphery(_eulerPeriphery);
+  }
+
+  // https://docs.chain.link/data-feeds/using-data-feeds
+  function getChainlinkDataFeedLatestAnswer() public view returns (int256) {
     // prettier-ignore
     (
       /* uint80 roundId */,
